5dc68ec43b727a8ee1bc8973249a9c563e73bc89
04alexklink <alex_klink123@hotmail.co.uk>
2020-10-18 17:31:47 +0100

    Added event listener for player selecting their move, R P or S, function logs in the console e.target.id at present

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,13 @@
-const choice = document.querySelectorAll('.choice');
+const choices = document.querySelectorAll('.choice');
 const score = document.getElementsByClassName('score');
 const result = document.getElementById('result');
 const restart = document.getElementById('restart');
+
+choices.forEach(choice => choice.addEventListener('click', play));
+// choice selection function
+function play(e) {
+    console.log(e.target.id);
+}
+
+
+// Event Listeners
