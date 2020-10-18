047e4009f8af58a76133625cd08dba4ae13e8ead
04alexklink <alex_klink123@hotmail.co.uk>
2020-10-18 13:53:41 +0100

    Created variables of elements to select from the DOM

---
+++ JS/main.js
@@ -0,0 +1,4 @@
+const choice = document.querySelectorAll('.choice');
+const score = document.getElementsByClassName('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
